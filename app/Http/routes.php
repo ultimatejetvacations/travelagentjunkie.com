582b2ce82c9fa46d19e417f5ef897999c5475b8d
Alejandro Perez <alejandro@ultimatejetvacations.com>
2015-04-08 16:26:12 -0400

    Fixed bad ssl links.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,13 +11,16 @@
 |
 */
 
-Route::post('quote/approve-option/{quoteOptionId}', ['as' => 'quote.approveOption', 'uses' => 'QuoteController@approveOption']);
-Route::post('quote/create-customer-profile', ['as' => 'quote.createCustomerProfile', 'uses' => 'QuoteController@createCustomerProfile']);
-Route::post('quote/create-post-sale', ['as' => 'quote.createPostSale', 'uses' => 'QuoteController@createPostSale']);
-Route::post('quote/save-traveler', ['as' => 'quote.saveTraveler', 'uses' => 'QuoteController@saveTraveler']);
-Route::post('quote/save-credit-card', ['as' => 'quote.saveCreditCard', 'uses' => 'QuoteController@saveCreditCard']);
-Route::get('quote/second-step/{token}', ['as' => 'quote.secondStep', 'uses' => 'QuoteController@secondStep']);
-Route::get('quote/{token}', ['as' => 'quote', 'uses' => 'QuoteController@quote']);
+Route::group(['https', 'prefix' => 'quote'], function()
+{
+    Route::post('approve-option/{quoteOptionId}', ['as' => 'quote.approveOption', 'uses' => 'QuoteController@approveOption']);
+    Route::post('create-customer-profile', ['as' => 'quote.createCustomerProfile', 'uses' => 'QuoteController@createCustomerProfile']);
+    Route::post('create-post-sale', ['as' => 'quote.createPostSale', 'uses' => 'QuoteController@createPostSale']);
+    Route::post('save-traveler', ['as' => 'quote.saveTraveler', 'uses' => 'QuoteController@saveTraveler']);
+    Route::post('save-credit-card', ['as' => 'quote.saveCreditCard', 'uses' => 'QuoteController@saveCreditCard']);
+    Route::get('second-step/{token}', ['as' => 'quote.secondStep', 'uses' => 'QuoteController@secondStep']);
+    Route::get('{token}', ['as' => 'quote', 'uses' => 'QuoteController@quote']);
+});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
