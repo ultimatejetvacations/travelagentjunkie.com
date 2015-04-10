19d354f88fddd65e5a33df8accf964b2448c737c
Alejandro Perez <alejandro@ultimatejetvacations.com>
2015-04-10 16:18:15 -0400

    Testing routes.

--- resources/views/quote/index.blade.php
+++ resources/views/quote/index.blade.php
@@ -379,7 +379,7 @@
 @section('content')
     {{--Modal window--}}
     @foreach($options as $key => $option)
-        {!! \Form::open(['method' => 'POST', 'url' => url('/quote/approve-option/'.$option->quote_option_id)]) !!}
+        {!! \Form::open(['method' => 'POST', 'url' => secure_url('/quote/approve-option/'.$option->quote_option_id)]) !!}
         {{--<form action="{{route('quote.approveOption', $option->quote_option_id)}}" method="POST">--}}
             {{--<input type="hidden" name="_token" value="{{csrf_token()}}">--}}
 
