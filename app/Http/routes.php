20e2dd48b3979d1c1537afd1e4a23e337028ac05
Alejandro Perez <alejandro@ultimatejetvacations.com>
2015-04-10 16:09:54 -0400

    Testing routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::group(['https', 'prefix' => 'quote'], function()
+Route::group(['prefix' => 'quote'], function()
 {
     Route::post('approve-option/{quoteOptionId}', ['as' => 'quote.approveOption', 'https', 'uses' => 'QuoteController@approveOption']);
     Route::post('create-customer-profile', ['as' => 'quote.createCustomerProfile', 'uses' => 'QuoteController@createCustomerProfile']);
